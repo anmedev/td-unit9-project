5f11b79be586d41a452f30bec3be61853fbe1d27
Anelle Mensah <aam2ds@virginia.edu>
2023-11-29 17:59:21 -0500

    add associations for models

--- models/course.js
+++ models/course.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Course.belongsTo(models.User, { 
+        foreignKey: {
+          fieldName: 'userId',
+          allowNull: false
+        },
+      });
     }
   }
   Course.init({
